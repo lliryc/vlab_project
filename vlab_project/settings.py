872e358e0979a30dec9db6e067ede6e766225f2d
lliryc <kirill.chirkunov@gmail.com>
2018-11-07 18:44:40 +0500

    Update settings.py

--- vlab_project/settings.py
+++ vlab_project/settings.py
@@ -182,8 +182,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'kirill.chirkunov@gmail.com'
-EMAIL_HOST_PASSWORD = 'pl,okm123'
+EMAIL_HOST_USER = 'emailaddress'
+EMAIL_HOST_PASSWORD = 'emailpassword'
 
 #Captcha
 CAPTCHA_FIELD_TEMPLATE = TEMPLATE_DIR + '\\captcha\\field.html'
